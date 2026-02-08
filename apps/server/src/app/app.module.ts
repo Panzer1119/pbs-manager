8b107664a54dba86a050b6484d9dbd342324ff9a
Paul Hagedorn <panzer1119@gmail.com>
2026-02-08 20:06:51 +0100

    feat(server): integrate global configuration and TypeORM setup

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import configuration from "./config/configuration";
+import databaseConfig from "./config/database.config";
+import pushoverConfig from "./config/pushover.config";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+            load: [configuration, databaseConfig, pushoverConfig],
+        }),
+        TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
