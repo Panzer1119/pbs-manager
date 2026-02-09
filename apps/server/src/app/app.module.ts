85e7f89e9207b3fa9656717197ba78851cc4a0e3
Paul Hagedorn <panzer1119@gmail.com>
2026-02-10 00:48:17 +0100

    feat(server): integrate ScheduleModule into app module

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import configuration from "./config/configuration";
 import databaseConfig from "./config/database.config";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
     imports: [
@@ -13,6 +14,7 @@ import databaseConfig from "./config/database.config";
             cache: true,
             load: [configuration, databaseConfig],
         }),
+        ScheduleModule.forRoot(),
         TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
     ],
     controllers: [AppController],
