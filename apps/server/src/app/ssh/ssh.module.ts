d4c0ba3870862b2b3840ef8ba1cb55cb6fa4e1ed
Paul Hagedorn <panzer1119@gmail.com>
2026-02-10 01:24:16 +0100

    feat(server): integrate BullMQ and Bull Board into SSH module

--- apps/server/src/app/ssh/ssh.module.ts
+++ apps/server/src/app/ssh/ssh.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { SSHService } from "./ssh.service";
 import { SSHProcessor } from "./ssh.processor";
+import { BullModule } from "@nestjs/bullmq";
+import { BullBoardModule } from "@bull-board/nestjs";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 
 @Module({
+    imports: [
+        BullModule.registerQueue({ name: SSHProcessor.QUEUE_NAME }),
+        BullBoardModule.forFeature({ name: SSHProcessor.QUEUE_NAME, adapter: BullMQAdapter }),
+    ],
     providers: [SSHService, SSHProcessor],
     exports: [SSHService],
 })
