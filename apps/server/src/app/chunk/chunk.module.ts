67b1a2d98b19c34a290b04ba380b47b1cace3817
Paul Hagedorn <panzer1119@gmail.com>
2026-02-10 15:40:44 +0100

    feat(api): generate controller Chunk
    nx g @nx/nest:controller --path="apps/server/src/app/chunk/chunk" --no-interactive
    
    install_and_commit "apps/server/src/app" "chunk"

--- apps/server/src/app/chunk/chunk.module.ts
+++ apps/server/src/app/chunk/chunk.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ChunkService } from "./chunk.service";
+import { ChunkController } from "./chunk.controller";
 
 @Module({
     providers: [ChunkService],
+    controllers: [ChunkController],
 })
 export class ChunkModule {}
