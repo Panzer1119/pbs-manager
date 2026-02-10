58c44e7246dabc52c65d4bddcb9779401bd15b52
Paul Hagedorn <panzer1119@gmail.com>
2026-02-10 09:18:56 +0100

    feat(server): implement SSH service initialization with queue draining functionality

--- apps/server/src/app/ssh/ssh.service.ts
+++ apps/server/src/app/ssh/ssh.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { InjectQueue } from "@nestjs/bullmq";
+import { SSHProcessor } from "./ssh.processor";
+import { Queue } from "bullmq";
 
 @Injectable()
-export class SSHService {}
+export class SSHService implements OnModuleInit {
+    private readonly logger: Logger = new Logger(SSHService.name);
+
+    constructor(
+        @InjectQueue(SSHProcessor.QUEUE_NAME)
+        private readonly sshQueue: Queue
+    ) {}
+
+    async onModuleInit(): Promise<void> {
+        await this.drainQueue();
+    }
+
+    async drainQueue(): Promise<void> {
+        try {
+            await this.sshQueue.drain(true);
+            this.logger.verbose(`Drained ${JSON.stringify(SSHProcessor.QUEUE_NAME)} queue`);
+        } catch (error) {
+            this.logger.error(`Failed to drain ${JSON.stringify(SSHProcessor.QUEUE_NAME)} queue: ${error}`);
+        }
+    }
+}
